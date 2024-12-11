011d3308229d6dabb4e4af042e41c29098dbb8fe
Olivier FAURE <couteaubleu@gmail.com>
2024-12-11 11:53:35 +0100

    Create crate

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+// Copyright 2024 Olivier FAURE
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! API for pushing formatted chunks of SQL to an SQLX QueryBuilder
+
+use sqlx::{Database, QueryBuilder};
+
+fn push_fragment<'args, DB: Database>(
+    query: &mut QueryBuilder<'args, DB>,
+    fragment: QueryBuilder<'args, DB>,
+) {
+    //let fragment = fragment.build();
+    //fragment.take_arguments().unwrap();
+
+    //query.push_sql(fragment);
+}
